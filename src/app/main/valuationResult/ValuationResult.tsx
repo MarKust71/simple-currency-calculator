01bff1aa3ebd5dd313370f5b84766cfca3064446
Marek Kustosz <markust71@hotmail.com>
2022-01-23 19:16:12 +0100

    add ValuationResult component move setting currency name deeper

---
+++ src/app/main/valuationResult/ValuationResult.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useTheme } from '@mui/material';
+
+import { ValuationResultProps } from './ValuationResult.types';
+import { useStyles } from './ValuationResult.styles';
+
+export const ValuationResult: React.FC<ValuationResultProps> = ({}) => {
+  const theme = useTheme();
+  const classes = useStyles(theme);
+
+  return <>ValuationResult</>;
+  };
