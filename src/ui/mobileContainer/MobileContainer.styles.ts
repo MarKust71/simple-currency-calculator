78d441bb14a45b9045f129cd2dc63afccebede12
Marek Kustosz <markust71@hotmail.com>
2022-01-24 15:27:05 +0100

    adjust desktop layout

--- src/ui/mobileContainer/MobileContainer.styles.ts
+++ src/ui/mobileContainer/MobileContainer.styles.ts
@@ -4,6 +4,7 @@ import { Theme } from '@mui/material';
 export const useStyles = makeStyles((theme: Theme) => ({
   container: {
     padding: theme.spacing(14, 2, 2),
+    backgroundColor: theme.palette.background.paper,
   },
   appBar: {
     padding: theme.spacing(2),
