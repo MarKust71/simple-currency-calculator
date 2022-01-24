5af8f35ef1d71765770d4aad59080e517c780bd3
Marek Kustosz <markust71@hotmail.com>
2022-01-24 17:37:34 +0100

    move currency name to hook

--- src/app/main/valuationResult/ValuationResult.styles.ts
+++ src/app/main/valuationResult/ValuationResult.styles.ts
@@ -2,5 +2,9 @@ import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material';
 
 export const useStyles = makeStyles((theme: Theme) => ({
-  wrapper: {},
+  inputWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 }));
